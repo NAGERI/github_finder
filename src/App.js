499a0a7c1198d1dee9205dc9c96af4f8c7c322c6
NAGERI <onacedricnageri@gmail.com>
2019-06-12 19:57:53 +0300

    Practise of conditionals and expressions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "ONA NAGERI";
+    const loading = false;
+    // return React.createElement(
+    //   "div",   without JSX
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hello from a react element")
+    // );
     return (
       <div className='App'>
-        <h1>Hello from react ONA</h1>
+        <Fragment>
+          {loading ? <h4>Loading...</h4> : <h1>Hello from react {name} </h1>}
+        </Fragment>
       </div>
     );
   }
