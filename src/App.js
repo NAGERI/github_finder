e34e139a24a7d964915c03b9e98c6a88fa5c474b
SPIF KLA - Cedric Nageri <Cedric.Nageri@spedaginterfreight.lan>
2022-09-23 09:05:37 +0300

    refactored,added githubapi, search ft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { Fragment, Component } from "react";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  foo = x => x + " bars from foo method"; /* this is an arrow function*/
+  // foo = (x) => x + " bars from foo method"; /* this is an arrow function*/
+  state = {
+    users: [],
+    loading: false,
+  };
+  /* Display the first 30 github users
+  async componentDidMount() {
+    this.setState({ loading: true });
+    
+    const res = await axios.get(
+      `http://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      
+      this.setState({ users: res.data, loading: false });
+    }*/
+  /**Search github users */
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    try {
+      const res = await axios.get(
+        `http://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      this.setState({ users: res.data.items, loading: false });
+    } catch (e) {
+      console.log("Error occured in axios API, GET request :", e);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
   render() {
     // const name = "Siogeri";
     // const loading = false;
@@ -12,17 +43,18 @@ class App extends Component {
     //   "div",   without JSX
     //   { className: "App" },
     //   React.createElement("h1", null, "Hello from a react element")
+    // {showName && name} conditional, shows name if showName variable is true
     // );
 
     return (
-      <div className='App'>
+      <div className="App">
         <Fragment>
           {/* {loading ? <h4>Loading...</h4> : <h1>Hello from react {name} </h1>} */}
-          <Navbar icon='fab fa-github' />
-          <div className='container'>
-            <Users />
+          <Navbar icon="fab fa-github" />
+          <div className="container">
+            <Search searchUsers={this.searchUsers} />
+            <Users loading={this.state.loading} users={this.state.users} />
           </div>
-
           {/* <h1>This is {this.foo(21)}</h1>{" "} */}
           {/*this is a class method  comments must be put in this */}
         </Fragment>
--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,6 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class Useritem extends Component {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  /** the above destructuring is similar to   const { login, avatar_url, html_url } = props.user; */
+  // functional components, pass the props instead of this.props key words
   // constructor() {
   //   super();
   //   console.log("I am a constructor");
@@ -17,25 +20,25 @@ class Useritem extends Component {
   //   avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
   //   html_url: "https://github.com/mojombo"
   // };
-  render() {
-    const { login, avatar_url, html_url } = this.props.user; // destructuting
-    return (
-      <div className='card text-center '>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More about {login}
-          </a>
-        </div>
+  return (
+    <div className="card text-center ">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More about {login}
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default Useritem;
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+export default UserItem;
